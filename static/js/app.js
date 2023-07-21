506b6b9ac721591c1b92544c371aea1566b36fb4
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-21 17:49:30 +0300

    check birthdat condition

--- static/js/app.js
+++ static/js/app.js
@@ -71,7 +71,9 @@ class PersonalContact extends Contact {
         const age = today.getFullYear() - dob.getFullYear();
         const monthDiff = today.getMonth() - dob.getMonth();
 
-
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+            return age - 1;
+        }
         return age;
     }
 }
