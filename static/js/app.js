05e50079f4ca9e3f4151cd67523aae36a098a12b
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-21 17:45:25 +0300

    private user contact called on contact class

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,9 @@
 class Contact {
     constructor(firstName, lastName, phoneNumber, emailAddress) {
-
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._phoneNumber = phoneNumber;
+        this._emailAddress = emailAddress;
     }
 }
 
